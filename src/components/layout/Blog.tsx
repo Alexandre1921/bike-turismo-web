c9ae9621670fee6b78ed36e777d851feb4a48a4f
andreruizrt <andrelucasruizalmeida@gmail.com>
2021-03-26 21:48:23 -0300

    blog grid

--- src/components/layout/Blog.tsx
+++ src/components/layout/Blog.tsx
@@ -1,37 +1,44 @@
 import React from 'react';
 
-import { Box } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 import PostCard from '../PostCard';
 
  
 const Blog: React.FC = () => {
   return (
-    <Box
-      padding={4}
-      w="100%"
-      maxW="900px"
-      mx="auto"
-      sx={{ columnCount: [1, 2, 3], columnGap: "8px" }}
+    <Grid
+      h="100px"
+      w="65rem"
+      justifyContent="center"
+      templateRows="repeat(2, 1fr)"
+      templateColumns="repeat(4, 1fr)"
+      gap={3}
     >
-      <PostCard
-        title="Onde comprar seu caça F-16?"
-        description="Hoje no Globo Reporter"
-        source={"https://homepages.cae.wisc.edu/~ece533/images/airplane.png"}
+      <GridItem rowSpan={2} colSpan={1} >
+        <PostCard
+          title="Onde comprar seu caça F-16?"
+          description="Hoje no Globo Reporter"
+          source={"https://homepages.cae.wisc.edu/~ece533/images/airplane.png"}
+          />
+      </GridItem> 
+      
+      <GridItem rowSpan={2} colSpan={1} >
+        <PostCard
+          title="Paisagens bonitas para conhecer de Bike"
+          description="Hoje no Globo Reporter"
+          source={"https://homepages.cae.wisc.edu/~ece533/images/fruits.png"}
         />
+      </GridItem>
 
-      <PostCard
-        title="Paisagens bonitas para conhecer de Bike"
-        description="Hoje no Globo Reporter"
-        source={"https://homepages.cae.wisc.edu/~ece533/images/arctichare.png"}
-        />
-
-      <PostCard
-        title="Melhores trilhas de 2020"
-        description="venha conhecer"
-        source={"https://homepages.cae.wisc.edu/~ece533/images/baboon.png"}
-        />
-    </Box>
+      <GridItem rowSpan={2} colSpan={1} >
+        <PostCard
+          title="Melhores trilhas de 2020"
+          description="venha conhecer"
+          source={"https://homepages.cae.wisc.edu/~ece533/images/barbara.png"}
+          />
+      </GridItem>
+    </Grid>
   );
 };
 
