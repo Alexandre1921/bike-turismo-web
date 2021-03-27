27e109691b172e164e177183ff0384aec233b6f8
Alexandre1921 <alexandre_leonardo2010@hotmail.com>
2021-03-26 23:07:30 -0300

    Teste deploy

--- next.config.js
+++ next.config.js
@@ -2,16 +2,13 @@ const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   env: {
-    asdasd:"ada",
-    serviceAccount: {
-      apiKey: process.env.FIREBASE_APIKEY,
-      authDomain: process.env.FIREBASE_AUTHDOMAIN,
-      projectId: process.env.FIREBASE_PROJECTID,
-      storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
-      appId: process.env.FIREBASE_APPID,
-      measurementId: process.env.FIREBASE_MEASUREMENTID
-    }
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.FIREBASE_APPID,
+    measurementId: process.env.FIREBASE_MEASUREMENTID
   },
   pwa: {
     disable:
