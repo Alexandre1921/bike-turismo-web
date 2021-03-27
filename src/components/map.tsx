bf42fc6dee5bc0408ec6feef57dcfeff65d1be8b
Alexandre1921 <alexandre_leonardo2010@hotmail.com>
2021-03-26 23:58:38 -0300

    Mapa aparecendo

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,14 +1,19 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 
 const Map = () => {
-  return (
-    <MapContainer zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-    </MapContainer>
-  );
+    const position = {lat: 51.505, lng:-0.09};
+
+    return (
+        <div style={{ width:"200px", height:"200px" }}>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        </div>
+        
+    );
 };
 
 export default Map;
