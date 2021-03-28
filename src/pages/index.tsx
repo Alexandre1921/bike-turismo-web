404a0892e91fed52d59eb96b56e672a1f925a9cd
andreruizrt <andrelucasruizalmeida@gmail.com>
2021-03-28 18:37:42 -0300

    search component in the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import { Box } from "@chakra-ui/layout";
 
 import Login from "../components/LoginButtons";
 import Blog from "../components/layout/Blog";
+import Search from "../components/Search";
 
 
 const Home = () => {
   return (
     <Box mb={8} w="full">
+      <Search placeholder="Buscar trilha..."/>
       <Blog />
     </Box>
   );
