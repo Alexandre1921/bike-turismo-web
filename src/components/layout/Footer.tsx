341088176b5ac932d13da33e49d07c06d46fbb73
andreruizrt <andrelucasruizalmeida@gmail.com>
2021-03-28 21:11:10 -0300

    fix footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,20 +10,11 @@ const Footer = () => {
 
   return (
     <Flex as="footer" width="full" align="center">
-<<<<<<< HEAD
       <Link href="https://github.com/Alexandre1921/bike-turismo-web" isExternal>
         <Button isFullWidth={isSmallerThan600} leftIcon={<AiFillGithub />}>
           Open in Github
         </Button>
       </Link>
-=======
-      {/* <Text>
-        2021 -{" "}
-        <Link href="https://sznm.dev" isExternal>
-          sznm.dev
-        </Link>
-      </Text> */}
->>>>>>> main
     </Flex>
   );
 };
