03aaea78711d1add783a8445665e4dbce9110cbc
andreruizrt <andrelucasruizalmeida@gmail.com>
2021-03-29 01:14:02 -0300

    background styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Box } from "@chakra-ui/layout";
 
-import Login from "../components/LoginButtons";
 import Blog from "../components/layout/Blog";
 import Search from "../components/Search";
 
