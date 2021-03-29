08d17fc8a3934ce9d6518dbf269384ad43c780d6
andreruizrt <andrelucasruizalmeida@gmail.com>
2021-03-29 00:02:03 -0300

    header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,49 +1,52 @@
 import { Button } from "@chakra-ui/button";
-import { Box, Flex, Heading } from "@chakra-ui/layout";
+import { Box, Heading } from "@chakra-ui/layout";
 
 // Some React Icons For Testing
 import { FaSearch } from 'react-icons/fa';
 import { GiRank3 } from 'react-icons/gi';
 import { RiRoadMapLine } from 'react-icons/ri';
 
+import { BiLogIn } from 'react-icons/bi'; 
+import { FaWpforms } from 'react-icons/fa'; 
+
+
 // TODO: @media resposive
-import { GiHamburgerMenu } from 'react-icons/gi';
+// import { GiHamburgerMenu } from 'react-icons/gi';
 
 import AccessibleLink from "../AccessibleLink";
-import Login from "../LoginButtons";
 import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
   return (
-    <Flex 
-      as="header" 
-      width="full" 
-      align="center" 
-      justifyContent={"space-between"}
-    >
-      <Box marginRight="auto">
-        <AccessibleLink href="/">
-          <Button background={"transparent"} leftIcon={<FaSearch />}>
-            <Heading as="h1">Buscar</Heading>
-          </Button>
-        </AccessibleLink>
-        <AccessibleLink href="/">
-          <Button background={"transparent"} leftIcon={<GiRank3 />}>
-            <Heading as="h1">Ranking</Heading>
-          </Button>
-        </AccessibleLink>
-        <AccessibleLink href="/map">
-          <Button background={"transparent"} leftIcon={<RiRoadMapLine />}>
-            <Heading as="h1">Map</Heading>
-          </Button>
-        </AccessibleLink>
-        <ThemeToggle />
-      </Box>
-
-      <Box p={0} marginLeft="auto">
-        <Login />
-      </Box>
-    </Flex>
+    <Box id="myTopnav" className="Header">
+      {/* <ThemeToggle /> */}
+      
+      <AccessibleLink href="/">
+        <Button className="nav" background="transparent" leftIcon={<FaSearch />}>
+          <Heading as="h1">Buscar</Heading>
+        </Button>
+      </AccessibleLink>
+      <AccessibleLink href="/ranking">
+        <Button className="nav" background="transparent" leftIcon={<GiRank3 />}>
+          <Heading as="h1">Ranking</Heading>
+        </Button>
+      </AccessibleLink>
+      <AccessibleLink href="/map">
+        <Button className="nav" background="transparent" leftIcon={<RiRoadMapLine />}>
+          <Heading as="h1">Map</Heading>
+        </Button>
+      </AccessibleLink>
+      <AccessibleLink href="/login">
+        <Button leftIcon={<BiLogIn />}>
+          Login
+        </Button>
+      </AccessibleLink>
+      <AccessibleLink href="/signup">
+        <Button leftIcon={<FaWpforms />}>
+          Sign out
+        </Button>
+      </AccessibleLink>
+    </Box>
   );
 };
 
