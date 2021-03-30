a7bdbe6d2bcd7583e4c2b6e83fa560cf1e841f05
Alexandre1921 <alexandre_leonardo2010@hotmail.com>
2021-03-29 23:45:24 -0300

    Separando blog e traduzindo 404

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@chakra-ui/layout";
 
+import Blog from "components/layout/Blog";
 import Search from "components/Search";
 
 
 const Home = () => {
   return (
     <Box mb={8} w="full">
-      <Search placeholder="Buscar trilha pelo nome"/>
+      <Search placeholder="Buscar post pelo nome"/>
+      <Blog />
     </Box>
   );
 };
