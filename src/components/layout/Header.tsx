a034d3446a3985cc625df4188504f07dddd82fda
andreruizrt <andrelucasruizalmeida@gmail.com>
2021-03-31 22:29:38 -0300

    merge header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,31 +1,15 @@
-<<<<<<< HEAD
-import { Button } from "@chakra-ui/button";
-import { Box, Heading } from "@chakra-ui/layout";
-=======
 import React from "react";
 import { Box, Heading, Flex, Text, Button, FlexProps, Spinner, HStack, Divider } from "@chakra-ui/react";
 import { useAuth } from "hooks/auth";
 import { EditIcon, StarIcon, SearchIcon } from '@chakra-ui/icons'
 import ThemeToggle from "./ThemeToggle";
 import Link from 'next/link'
->>>>>>> 141af96c03ed9a8d7165eb34e665ebb2d1b444d8
 
 type MenuItemsProps = {
   children: React.ReactNode;
   href: string;
 };
 
-<<<<<<< HEAD
-import { BiLogIn } from 'react-icons/bi'; 
-import { FaWpforms } from 'react-icons/fa'; 
-
-
-// TODO: @media resposive
-// import { GiHamburgerMenu } from 'react-icons/gi';
-
-import AccessibleLink from "../AccessibleLink";
-import ThemeToggle from "./ThemeToggle";
-=======
 const MenuItems: React.FC<MenuItemsProps> = ({ children, href }: MenuItemsProps) => (
   <Link href={href}>
     <Box mt={{ base: 4, md: 0 }} mr={6} display="block" cursor="pointer">
@@ -38,40 +22,8 @@ const Header: React.FC<FlexProps> = (props: FlexProps) => {
   const [show, setShow] = React.useState(false);
   const handleToggle = () => setShow(!show);
   const { user, userDataPresent, signOut } = useAuth();
->>>>>>> 141af96c03ed9a8d7165eb34e665ebb2d1b444d8
 
   return (
-<<<<<<< HEAD
-    <Box id="myTopnav" className="Header">
-      {/* <ThemeToggle /> */}
-      
-      <AccessibleLink href="/">
-        <Button className="nav" background="transparent" leftIcon={<FaSearch />}>
-          <Heading as="h1">Buscar</Heading>
-        </Button>
-      </AccessibleLink>
-      <AccessibleLink href="/ranking">
-        <Button className="nav" background="transparent" leftIcon={<GiRank3 />}>
-          <Heading as="h1">Ranking</Heading>
-        </Button>
-      </AccessibleLink>
-      <AccessibleLink href="/map">
-        <Button className="nav" background="transparent" leftIcon={<RiRoadMapLine />}>
-          <Heading as="h1">Map</Heading>
-        </Button>
-      </AccessibleLink>
-      <AccessibleLink href="/login">
-        <Button leftIcon={<BiLogIn />}>
-          Login
-        </Button>
-      </AccessibleLink>
-      <AccessibleLink href="/signup">
-        <Button leftIcon={<FaWpforms />}>
-          Sign out
-        </Button>
-      </AccessibleLink>
-    </Box>
-=======
     <Flex
       as="nav"
       align="center"
@@ -150,7 +102,6 @@ const Header: React.FC<FlexProps> = (props: FlexProps) => {
       </Box>
       
     </Flex>
->>>>>>> 141af96c03ed9a8d7165eb34e665ebb2d1b444d8
   );
 };
 
