e718574b06926cb93ead96a9212ff98e35441be6
Alexandre1921 <alexandre_leonardo2010@hotmail.com>
2021-04-03 15:49:55 -0300

    Listando rank

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,32 +20,52 @@ import BrazilianLocale from "date-fns/locale/pt-BR";
 
 const Home = () => {
   const mapId ="xXLSMAvhyOKFXR4TFV2P";
-  const [route, setRoute] = useState<IRoute>();
+  const [routes, setRoutes] = useState<Array<IRoute>>([]);
   const ref = useRef(null);
   console.log(ref.current);
   const href = `/map?mapId=${mapId}`;
 
   useEffect(()=>{
-    mapId && db.collection("routes").doc(mapId || "").get()
-    .then(doc => doc.exists ? doc.data() as IRoute : Promise.reject("Rota não encontrada"))
-    .then(data => Promise.all(
-          data.pointers.map(pointer => 
-            storage.ref(`/routes/${mapId}/${pointer.avatar_url}`)
-            .getDownloadURL()
-        )
-      ).then((pointersUrl) => 
-        storage.ref(`/routes/${mapId}/${data.avatar_url}`)
-        .getDownloadURL()
-        .then((res) =>
-          setRoute({
+    mapId && db.collection("routes").orderBy("stars").limit(5).get()
+    .then(query => query.empty ? Promise.reject("Nenhuma rota encontrada") : query.docs)
+    .then(docs => docs.map(doc => {
+      const data = doc.data() as IRoute;
+      const getDownloadUrl = (avatar_url: string) => storage.ref(`/routes/${mapId}/${avatar_url}`).getDownloadURL();
+      
+      Promise.all(data.pointers.map(pointer => getDownloadUrl(pointer.avatar_url)))
+      .then(pointersUrl => 
+        getDownloadUrl(data.avatar_url)
+        .then(res =>
+          setRoutes(oldRoutes => ([...oldRoutes, {
             ...data,
             avatar_url: res,
             pointers: data.pointers.map((pointer, index) => ({...pointer, avatar_url: pointersUrl[index]}) )
-          })
-        )
-      )
-    );
+          }]))
+      ));
+    }));
   }, [mapId]);
+
+  // useEffect(()=>{
+    // mapId && db.collection("routes").doc(mapId || "").get()
+    // .then(doc => doc.exists ? doc.data() as IRoute : Promise.reject("Rota não encontrada"))
+    // .then(data => Promise.all(
+    //       data.pointers.map(pointer => 
+    //         storage.ref(`/routes/${mapId}/${pointer.avatar_url}`)
+    //         .getDownloadURL()
+    //     )
+    //   ).then((pointersUrl) => 
+    //     storage.ref(`/routes/${mapId}/${data.avatar_url}`)
+    //     .getDownloadURL()
+    //     .then((res) =>
+    //       setRoute({
+    //         ...data,
+    //         avatar_url: res,
+    //         pointers: data.pointers.map((pointer, index) => ({...pointer, avatar_url: pointersUrl[index]}) )
+    //       })
+    //     )
+    //   )
+    // );
+  // }, [mapId]);
 
   const Map = useMemo(() => dynamic(
     () => import('components/map'), // replace '@components/map' with your component's location
@@ -56,25 +76,22 @@ const Home = () => {
   ), [/* list variables which should trigger a re-render here */]);
 
   const color = useColorModeValue("white", "gray.800");
-
-  route && console.log(route.created_at.toDate());
-
+    console.log(routes);
   return (
     <Box mx={2} mb={8} flex="1">
       <Search placeholder="Buscar trilha pelo nome"/>
       <Divider my={3}></Divider>
-      <Heading>Ranking das trilhas</Heading>
+      <Heading>Top 5 trilhas</Heading>
             <Tabs>
               <TabList>
-                <Tab _focus={{outline:"none"}}>#1 Lugar</Tab>
+                {routes.map((route, index) => <Tab _focus={{outline:"none"}} key={index}>#{index+1} Lugar</Tab>)}
               </TabList>
-              {route && (
+              <TabPanels>
+              {routes.map((route, index)=>
+                <TabPanel key={index}>
                 <Box mt={2} mx={5} borderRadius={5}>
                   <Map route={route} reference={db.collection("routes").doc(mapId || "") as firebase.firestore.DocumentReference<IRoute>} />
                 </Box>
-              )}
-              <TabPanels>
-                <TabPanel>
                 <Box mt={1} bgColor={color} borderRadius="0.375rem">
               <LinkBox as="article" p="5" borderWidth="1px" rounded="md">
                 {route?.updated_at ? (
@@ -112,6 +129,7 @@ const Home = () => {
               </LinkBox>
             </Box>
                 </TabPanel>
+              )}
               </TabPanels>
             </Tabs>
     </Box>
